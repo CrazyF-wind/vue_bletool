79f8cfc7ea5e4eb89a6ad4d9d4ab08ef8ebd0511
Fwind <ff1990@126.com>
2017-07-31 14:53:11 +0800

    1、新曾距离列表组件

---
+++ src/components/distance.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-form-item label="距离（米）">
+      <el-select v-model="distance" @change="getDistanceValue" placeholder="请选择">
+        <el-option
+          v-for="item in distances"
+          :label="item.label"
+          :value="item.value"
+          :key="item.value">
+        </el-option>
+      </el-select>
+    </el-form-item>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import qs from 'qs'
+
+  export default {
+    data () {
+      return {
+        distance: '',
+        distances: []
+      }
+    },
+    created () {
+      /**
+       * 加载测试距离
+       */
+      this.$http.post('/ble_get_distance', qs.stringify({'userid': ''})).then(response => {
+        let distanceList = response.data.data.distance_list
+        let distanceCache = []
+        console.log(`distance:${distanceList}`)
+        distanceList.forEach(function (val) {
+          distanceCache.push({'label': val, 'value': val})
+        })
+        this.distances = distanceCache
+        this.distance = distanceList[0]
+      })
+    },
+    methods: {
+      getDistanceValue () {
+        this.$emit('getDistance', this.distance)
+      }
+    }
+  }
+</script>

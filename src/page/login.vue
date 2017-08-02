c441bdc2b1006d93369308af8b5eeb81453731ce
Fwind <ff1990@126.com>
2017-08-02 17:41:52 +0800

    1、登录注册密码输入新增md5加密；

--- src/page/login.vue
+++ src/page/login.vue
@@ -3,7 +3,7 @@
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
       <span class="heading">用户登录</span>
       <el-form-item label="用户名" prop="username">
-        <el-input v-model.number="ruleForm.username"></el-input>
+        <el-input v-model="ruleForm.username"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="pass">
         <el-input type="password" v-model="ruleForm.pass" auto-complete="off"></el-input>
@@ -11,6 +11,7 @@
       <el-form-item>
         <el-button type="primary" @click="login()">登录</el-button>
         <el-button @click="resetForm('ruleForm')">重置</el-button>
+        <el-button @click="register">注册</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -19,6 +20,7 @@
 <script type="text/ecmascript-6">
   import qs from 'qs'
   import cookie from '../util/cookie'
+  import md5 from 'md5'
 
   export default {
     data () {
@@ -69,25 +71,28 @@
       login () {
         let params = {
           username: this.ruleForm.username,
-          pass: this.ruleForm.pass
+          password: md5(this.ruleForm.pass)
         }
-        this.$http.post('/login', qs.stringify(params)).then(res => {
-          console.log(JSON.stringify(res))
-          if (res['data']['code'] === 0) {
+        console.log(`params:${this.ruleForm.username}`)
+        this.$http.post('/users/login', qs.stringify(params)).then(res => {
+          console.log(`res.data:${JSON.stringify(res.data.data)}`)
+          if (res.data.status.code === 1010100) {
             // 用户名存入store vuex
             this.$store.commit('isLogin', this.ruleForm.username)
             // 用户名存入cookie
-            cookie.setCookie('username', res['data']['data']['username'], 365)
+            cookie.setCookie('username', res.data.data.username, 365)
             // 用户编号存入cookie
-            cookie.setCookie('userid', res['data']['data']['userid'], 365)
+            cookie.setCookie('userid', res.data.data.userid, 365)
             // 进入主页
             this.$router.push('/main')
           } else {
-            alert('密码错误')
+            this.$message.error(res.data.status.sub_msg, 'ERROR!')
           }
         })
+      },
+      register () {
+        this.$router.push('/register')
       }
-
     }
   }
 </script>
--- src/page/register.vue
+++ src/page/register.vue
@@ -1,11 +1,107 @@
 <template>
-    <div>
-
-    </div>
+  <div id="register-container">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <span class="heading">用户登录</span>
+      <el-form-item label="用户名">
+        <el-input v-model="ruleForm.userName"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="ruleForm.pass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="重新输入">
+        <el-input type="password" v-model="ruleForm.againPass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submit">提交</el-button>
+        <el-button @click="quit">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        props: []
+  import qs from 'qs'
+  import md5 from 'md5'
+
+  export default {
+    data () {
+      let checkUserName = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('用户名不能为空'))
+        }
+      }
+      let validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'))
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass')
+          }
+          callback()
+        }
+      }
+      return {
+        ruleForm: {
+          userName: '',
+          pass: '',
+          againPass: ''
+        },
+        form: {
+          userName: '',
+          pass: '',
+          againPass: ''
+        },
+        rules: {
+          pass: [
+            {validator: validatePass, trigger: 'blur'}
+          ],
+          againPass: [
+            {validator: validatePass, trigger: 'blur'}
+          ],
+          username: [
+            {validator: checkUserName, trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    methods: {
+      submit () {
+        let username = this.ruleForm.userName
+        let password = md5(this.ruleForm.pass)
+        this.$http.post('/users/register', qs.stringify({
+          'username': username,
+          'password': password
+        })).then(response => function () {
+          if (response.data.status.code === 1020100) {
+            this.$message.info('注册成功！', 'INFO!')
+          } else if (this.pass !== this.againPass) {
+            this.$message.warn('两次密码不一致！', 'WARN!')
+          } else {
+            console.log(`error:${response.data.status}`)
+            this.$message.error(response.data.status.sub_msg, 'ERROR!')
+          }
+        })
+      },
+      quit () {
+        this.$router.push('/login')
+      }
     }
+  }
 </script>
+
+<style>
+  #register-container {
+    display: flex;
+    align-items: center; /*定义body的元素垂直居中*/
+    justify-content: center; /*定义body的里的元素水平居中*/
+  }
+
+  .heading {
+    display: block;
+    font-size: 35px;
+    font-weight: 700;
+    padding: 35px 0 0 100px;
+    border-bottom: 1px solid #f0f0f0;
+    margin-bottom: 30px;
+  }
+</style>

2b7f3aa119f9b00f26c016881327da68e7b5121d
Fwind <ff1990@126.com>
2017-08-02 17:57:18 +0800

    1、更新距离参数组件；

--- src/components/distance.vue
+++ src/components/distance.vue
@@ -15,26 +15,30 @@
 
 <script type="text/ecmascript-6">
   import qs from 'qs'
+  import cookie from '../util/cookie'
 
   export default {
     data () {
       return {
         distance: '',
-        distances: []
+        distances: [],
+        userid: ''
       }
     },
     created () {
+      this.userid = cookie.getCookie('userid')
+
       /**
        * 加载测试距离
        */
-      this.$http.post('/ble_get_distance', qs.stringify({'userid': ''})).then(response => {
-        let distanceList = response.data.data.distance_list
+      this.$http.post('/config/ble_get_distance', qs.stringify({'userid': this.userid})).then(response => {
+        let distanceList = response.data.data
         let distanceCache = []
         distanceList.forEach(function (val) {
-          distanceCache.push({'label': val, 'value': val})
+          distanceCache.push({'label': val['distance'], 'value': val['distance']})
         })
         this.distances = distanceCache
-        this.distance = distanceList[0]
+        this.distance = distanceList[0]['distance']
       })
     },
     methods: {
